c68f32fc277a9848eba6bc910b8bc607a9d3193d
NupurRedlioDesigns <nupur.redliodesigns@gmail.com>
2024-04-15 15:02:21 +0530

    added demo pages

---
+++ pages/dashboard.js
@@ -0,0 +1,15 @@
+import Button from "@/components/Button/Button";
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Dashboard() {
+    <Head>
+        <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.svg" />
+        <title>Dashboard</title>
+    </Head>
+    return (
+        <>
+            <h1 className="font-extrabold font-poppins text-16 text-primary-60">Dashboard</h1>
+        </>
+    );
+}
---
+++ pages/profile.js
@@ -0,0 +1,14 @@
+
+import Head from "next/head";
+
+export default function Profile() {
+    <Head>
+        <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.svg" />
+        <title>Users</title>
+    </Head>
+    return (
+        <>
+            <h1 className="font-extrabold font-poppins text-16 text-primary-60">Dashboard</h1>
+        </>
+    );
+}
